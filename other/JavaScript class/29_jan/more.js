5e9ce8c60f50427efe0be411d3baa673e9a915fd
siddharth rukadikar <siddharthrukadikar@gmail.com>
2024-05-10 13:22:29 +0530

    js_class

---
+++ other/JavaScript class/29_jan/more.js
@@ -0,0 +1,95 @@
+// spread and rest
+
+// spread 
+// function sumone(x,y){
+//     return x+y;
+// }
+
+// let var1=[9,7]
+
+// console.log(sumone(...var1));
+
+// rest 
+
+// function sum(...args){
+//     let sum=0;
+//     for (const x of args) {
+//         sum=sum+x;
+//     }
+//     return sum;
+// }
+// console.log(sum(1,2,3,4,5,6,7,8,9,10));
+
+
+// try and catch 
+
+
+// try{
+//     let firstname="siddharth"
+//     console.log(firstname + "" + lastname);
+// }catch(err){
+//     console.log(err.name,"variable missing");
+// }finally{
+//     console.log("i will run");
+// }
+
+// throw =>explore
+
+// event loop 
+
+// const userone= ()=>{
+//     console.log("hello one");
+//     setTimeout(()=>{
+//         console.log("hello i am inside 1");
+//     },3000)
+// }
+// const usertwo= ()=>{
+//     setTimeout(()=>{
+//         console.log("hello i am inside 2");
+//     },3000)
+//     console.log("hello two");
+// }
+
+// const userthree= ()=>{
+//     console.log("hello three");
+//     setTimeout(()=>{
+//         console.log("hello i am inside 3");
+//     },3000)
+// }
+
+
+// userone();
+// usertwo();
+// userthree();
+
+// promise 
+
+// const One=()=>{
+//     return "i am one";
+// }
+
+// const two= () =>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("credential correct")
+//         },3000)
+//     });
+// }
+// const three= () =>{
+//     return "i am three";
+// }
+
+// const callMe=async  () =>{
+//     let valone=One();
+//     console.log(valone);
+
+//     let valtwo= await two();
+//     console.log(valtwo);
+
+//     let valthree=three();
+//     console.log(valthree);
+    
+// }
+
+// callMe()
+
---
+++ other/JavaScript class/4_feb/sat_script.js
@@ -0,0 +1,4 @@
+// console.log(a);
+var a=19;
+console.log(a);
+ 
